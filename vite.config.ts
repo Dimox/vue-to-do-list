cb794b55f8f48ccfa4d89265761c3edc8c6ea31d
Dimox <dimox.name@gmail.com>
2024-03-11 19:48:28 +0500

    chore: add vite-plugin-run, svg sprite, icon types generator

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import path from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { run } from 'vite-plugin-run'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    run([
+      {
+        name: 'svg sprite is changed',
+        run: ['pnpm generate-icon-types'],
+        pattern: ['src/assets/img/sprite.svg'],
+        startup: false,
+        build: false,
+      },
+    ]),
+    vue(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
