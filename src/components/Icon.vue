f6a68a9bd1e4d9e8fd57d3983289d7ee260f3616
Dimox <dimox.name@gmail.com>
2024-03-16 01:36:22 +0500

    feat(ui): add Icon component

---
+++ src/components/Icon.vue
@@ -0,0 +1,28 @@
+<template>
+  <svg :width="width" :height="height">
+    <use :xlink:href="iconUrl" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+import { computed, PropType } from 'vue'
+import { IconName } from '@/types/icons'
+import spriteUrl from '@/assets/img/sprite.svg?url'
+
+const props = defineProps({
+  name: {
+    type: String as PropType<`${IconName}`>,
+    default: '',
+  },
+  width: {
+    type: [String, Number],
+    default: 24,
+  },
+  height: {
+    type: [String, Number],
+    default: 24,
+  },
+})
+
+const iconUrl = computed(() => `${spriteUrl}#${props.name}`)
+</script>
