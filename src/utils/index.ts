05ce635d216ba525cd03b42070d7806d436cf2c2
Dimox <dimox.name@gmail.com>
2024-03-16 01:32:16 +0500

    feat(utils): add sortToDos

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+import { ToDoItem } from '@/types'
+
 export const uniqueId = (prefix: string = '') =>
   prefix + String(Date.now().toString(32) + Math.random().toString(16)).replace(/\./g, '')
 
@@ -5,4 +7,12 @@ export const textareaAutoHeight = (el: HTMLTextAreaElement) => {
   const style = window.getComputedStyle(el)
   el.style.height = 'auto'
   el.style.height = `${el.scrollHeight + parseInt(style.borderTopWidth) + parseInt(style.borderBottomWidth)}px`
+}
+
+export const sortToDos = (items: ToDoItem[]) => {
+  return items.sort((a, b) => {
+    if (a.checked === b.checked) return 0
+    if (a.checked) return 1
+    return -1
+  })
 }
