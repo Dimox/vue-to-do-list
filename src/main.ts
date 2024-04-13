657daf8d6f8c782d431142b708ea74887ed840c2
Dimox <dimox.name@gmail.com>
2024-04-13 18:19:45 +0500

    feat: set html 'lang' attribute on language change, move i18n messages to separate file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { useFavicon } from '@/composables/favicon'
+import { setHtmlLang } from '@/i18n'
 import '@/assets/scss/main.scss'
 
-createApp(App).use(useFavicon).mount('#app')
+createApp(App).use(setHtmlLang).use(useFavicon).mount('#app')
