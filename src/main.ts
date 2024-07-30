ac15162607dca6c2c0c87e0ddf18ef157406de88
Dimox <dimox.name@gmail.com>
2024-07-30 21:56:44 +0500

    feat(app): use useContrastingTextColor

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { useFavicon } from '@/composables/favicon'
+import { useContrastingTextColor } from '@/composables/contrasting-text-color'
 import { setHtmlLang } from '@/i18n'
 import '@/assets/scss/main.scss'
 
-createApp(App).use(setHtmlLang).use(useFavicon).mount('#app')
+createApp(App).use(setHtmlLang).use(useFavicon).use(useContrastingTextColor).mount('#app')
