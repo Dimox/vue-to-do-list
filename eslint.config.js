6507807a78936debf1b02d01ce6536d5dd2c8038
Dimox <dimox.name@gmail.com>
2024-07-31 10:07:43 +0500

    chore: update eslint, migrate to flat config

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import globals from 'globals'
+import eslint from '@eslint/js'
+import tsEslint from 'typescript-eslint'
+import vueEslintParser from 'vue-eslint-parser'
+import eslintPluginImport from 'eslint-plugin-import'
+import eslintPluginVue from 'eslint-plugin-vue'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+export default tsEslint.config(
+  ...eslintPluginVue.configs['flat/recommended'],
+  eslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    files: ['src/**/*.{ts,js,vue}'],
+    extends: [...tsEslint.configs.strictTypeChecked, ...tsEslint.configs.stylisticTypeChecked],
+    plugins: {
+      '@typescript-eslint': tsEslint.plugin,
+      import: eslintPluginImport,
+      vue: eslintPluginVue,
+    },
+    languageOptions: {
+      parser: vueEslintParser,
+      parserOptions: {
+        project: ['tsconfig.json'],
+        tsconfigRootDir: import.meta.dirname,
+        parser: tsEslint.parser,
+        sourceType: 'module',
+        ecmaVersion: 'latest',
+        globals: {
+          ...globals.browser,
+          ...globals.es2021,
+          ...globals.node,
+        },
+        extraFileExtensions: ['.vue'],
+      },
+    },
+    rules: {
+      'import/order': 'error',
+      'vue/multi-word-component-names': 'off',
+      'vue/no-v-html': 'off',
+    },
+  }
+)
