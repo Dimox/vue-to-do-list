856835cab071794182bd4a6e3a3cd6937cba7ee0
Dimox <dimox.name@gmail.com>
2024-12-01 19:58:41 +0500

    feat(storage): update default options, remove params from useToDoStorage

--- src/composables/storage.ts
+++ src/composables/storage.ts
@@ -3,16 +3,16 @@ import type { ToDoList, ToDoOptions } from '@/types'
 
 export const defaultOptions: ToDoOptions = {
   lang: 'en',
-  colorScheme: 'light',
+  colorScheme: 'auto',
   appWidth: '640',
   accentColor: '#4f46e5',
 }
 
-export const useToDoStorage = (data?: ToDoList) => {
+export const useToDoStorage = () => {
   const key = 'vue-to-do-list'
   const defaultData: ToDoList = {
     items: [],
     options: defaultOptions,
   }
-  return data ? useStorage(key, data) : useStorage(key, defaultData)
+  return useStorage(key, defaultData)
 }
